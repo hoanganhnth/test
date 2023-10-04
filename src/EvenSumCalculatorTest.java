1b79802b20e767455061d3ed98c2a2014a144225
hoanganhnth <hoanganhnth2k3@gmail.com>
2023-10-04 23:12:26 +0700

    test dong dieu khien

--- src/EvenSumCalculatorTest.java
+++ src/EvenSumCalculatorTest.java
@@ -1,9 +1,14 @@
 public class EvenSumCalculatorTest {
     public static void main(String[] args) {
 
-
+        //kiem thu chuc nang
         System.out.println("Test 1: " + EvenSumCalculator.calculateEvenSum(0)); // Kết quả đúng: 0.0
         System.out.println("Test 2: " + EvenSumCalculator.calculateEvenSum(5)); // Kết quả đúng: 6
         System.out.println("Test 3: " + EvenSumCalculator.calculateEvenSum(10)); // Kết quả đúng: 30
+
+        //kiem thu dong dieu khien
+        System.out.println("Test 4: " + EvenSumCalculator.calculateEvenSum(6)); // Kết quả đúng: 12
+        System.out.println("Test 5: " + EvenSumCalculator.calculateEvenSum(8)); // Kết quả đúng: 20
+
     }
 }
